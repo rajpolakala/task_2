16d0072a97c534a115c2139e2c34e7a0f8a14a48
rajpolakala <pushpabuzz@gmail.com>
2022-12-18 01:32:18 +0530

    Initial commit

---
+++ main.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  profile = "default"
+  region  = var.region
+}
+
+data "aws_default_vpc" "default" {
+  default = true
+}
+
+resource "aws_instance" "aws_ubuntu" {
+  instance_type          = var.instance_type
+  ami                    = var.ami
+  key_name               = var.key_name
+  user_data              = file("userdata.sh")
+}
+
+resource "aws_security_group" "demo_sg" {
+  name        = "demo_sg"
+  description = "allow ssh on 22 & http on port 80"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  default = "ap-southeast-1"
+}
+variable "instance_type" {
+  default = "t2.micro"
+}
+variable "ami" {
+  default = "ami-005835d578c62050d"
+}
